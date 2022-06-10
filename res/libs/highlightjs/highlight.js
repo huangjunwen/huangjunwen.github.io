dbf430fd657273cb217e6da004585933513e35e9
jayven <kassarar@gmail.com>
2022-06-10 18:39:52 +0800

    更新 hljs 版本

--- res/libs/highlightjs/highlight.js
+++ res/libs/highlightjs/highlight.js
@@ -1,6 +1,6 @@
 /*!
-  Highlight.js v11.3.1 (git: 2a972d8658)
-  (c) 2006-2021 Ivan Sagalaev and other contributors
+  Highlight.js v11.5.1 (git: b8f233c8e2)
+  (c) 2006-2022 Ivan Sagalaev and other contributors
   License: BSD-3-Clause
  */
 var hljs = (function () {
@@ -428,11 +428,13 @@ var hljs = (function () {
       }
     }
 
+    /** @typedef { {capture?: boolean} } RegexEitherOptions */
+
     /**
      * Any of the passed expresssions may match
      *
      * Creates a huge this | this | that | that match
-     * @param {(RegExp | string)[] } args
+     * @param {(RegExp | string)[] | [...(RegExp | string)[], RegexEitherOptions]} args
      * @returns {string}
      */
     function either(...args) {
@@ -1556,7 +1558,7 @@ var hljs = (function () {
       return mode;
     }
 
-    var version = "11.3.1";
+    var version = "11.5.1";
 
     class HTMLInjectionError extends Error {
       constructor(reason, html) {
@@ -1833,8 +1835,8 @@ var hljs = (function () {
          */
         function emitMultiClass(scope, match) {
           let i = 1;
-          // eslint-disable-next-line no-undefined
-          while (match[i] !== undefined) {
+          const max = match.length - 1;
+          while (i <= max) {
             if (!scope._emit[i]) { i++; continue; }
             const klass = language.classNameAliases[scope[i]] || scope[i];
             const text = match[i];
@@ -2290,7 +2292,8 @@ var hljs = (function () {
         if (element.children.length > 0) {
           if (!options.ignoreUnescapedHTML) {
             console.warn("One of your code blocks includes unescaped HTML. This is a potentially serious security risk.");
-            console.warn("https://github.com/highlightjs/highlight.js/issues/2886");
+            console.warn("https://github.com/highlightjs/highlight.js/wiki/security");
+            console.warn("The element with unescaped HTML:");
             console.warn(element);
           }
           if (options.throwUnescapedHTML) {
